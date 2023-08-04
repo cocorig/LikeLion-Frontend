1a1d6da7388a33de0e774bea29d5a288d590f5a1
Hayeon LEE <fhdodl@naver.com>
2023-08-04 23:06:29 +0900

    📝 : 230803 JS 실습

---
+++ JavaScript/230803/a.js
@@ -0,0 +1,8 @@
+function test () {
+  console.log('hello')
+  console.log('hello')
+  console.log('hello')
+}
+//var사용시 에러 안남
+let a = 1000;
+//Uncaught SyntaxError: Identifier 'a' has already been declared (at b.js:1:1)
