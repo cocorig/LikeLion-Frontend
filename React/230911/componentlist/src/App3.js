7a866d3e29ae603157a6f357ed9e96364bf9a7e6
Hayeon LEE <fhdodl@naver.com>
2023-09-12 23:56:53 +0900

    Feat : modal 기능

--- React/230911/componentlist/src/App3.js
+++ React/230911/componentlist/src/App3.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Login from './components/Login';
 import Homepage from './components/Homepage';
+import Modal from './Components/Modal';
 
 export default function App3() {
   // user 정보
@@ -11,11 +12,15 @@ export default function App3() {
 
     // 로그인 상태를 판단하는 state
     const [login, setLogin] = useState(false);
+    const [modalShow, setModalShow] = useState(true);
 
     // 처음엔 false니까 Login 컴포넌트 렌더링 Login컴포넌트에 user정보 객체와 로그인 상태를 저장하는 함수 setLogins를 props로 보낸다.
 
     // setLogin이 true면 , 로그인에 성공하면 Homepage렌더링
     return (
-        login ? <Homepage /> : <Login infoUser={user} setLogin={setLogin} />
+        <>
+        {login ? <Homepage setLogin={setLogin} /> : <Login infoUser={user} setLogin={setLogin} />}
+        {modalShow && <Modal setModalShow={setModalShow} />}
+    </>
     )
 }
