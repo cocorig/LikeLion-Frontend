13e48df675dda19abf06ac261884764d0e70e763
Hayeon LEE <fhdodl@naver.com>
2023-11-11 23:30:18 +0900

    feat : 키 이벤트를 사용해  player 움직임 구현

--- Inflearn-Lectures/JS-Sniper/Flappy-Bird/src/index.js
+++ Inflearn-Lectures/JS-Sniper/Flappy-Bird/src/index.js
@@ -3,25 +3,55 @@ const startBtn = document.querySelector(".startBtn");
 const gameArea = document.querySelector(".gameArea");
 const gameMessage = document.querySelector(".gameMessage");
 
-//유저의 키 정보를 저장하는 객체
 let keys = {};
+let ghost = document.createElement("div");
 
-// 게임시작을 알리는 함수
+// 유령의 정보를 담은 변수
+let player = {
+  x: 0,
+  y: 0,
+  speed: 2,
+};
+
 const start = () => {
-  console.log("시작");
   gameMessage.classList.add("hide");
   startBtn.classList.add("hide");
+  ghost.setAttribute("class", "ghost");
+  gameArea.appendChild(ghost);
+  // 현재 위치 초기화
+  player.x = ghost.offsetLeft;
+  player.y = ghost.offsetTop;
+  window.requestAnimationFrame(playGame);
 };
-// 키다운 이벤트를 처리하는 함수
+// requestAnimationFrame : 애니메이션을 부드럽게 처리해주는 함수
+
+const playGame = () => {
+  if (keys.ArrowLeft) {
+    player.x -= player.speed;
+  }
+  if (keys.ArrowRight) {
+    player.x += player.speed;
+  }
+  if (keys.ArrowUp) {
+    player.y -= player.speed;
+  }
+  if (keys.ArrowDown) {
+    player.y += player.speed;
+  }
+  ghost.style.left = player.x + "px";
+  ghost.style.top = player.y + "px";
+  window.requestAnimationFrame(playGame); // 여기서 자기 자신을 또 실행함(무한반복하게 됨)
+};
+
 const pressOn = (e) => {
-  console.log(e.code); // e.key를 사용할 수도 있지만 space 경우 공백으로 출력되기 때문에 code를 사용,
-  keys[e.code] = true; // keys객체의 key값으로 현재 눌린 키의 정보를 담음,  눌려진 키의 정보를 true로 설정하여 해당 키가 눌려졌음을 표시
+  console.log(e.code);
+  keys[e.code] = true;
   console.log(keys);
 };
-// 키업 이벤트를 처리하는 함수
+
 const pressOff = (e) => {
   console.log(e.code);
-  keys[e.code] = false; // 눌려진 키의 정보를 false로 설정하여 해당 키가 놓아졌음을 표시
+  keys[e.code] = false;
   console.log(keys);
 };
 
